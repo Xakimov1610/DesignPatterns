fc7f0b0bc044d8fd0537f05820764363bdd9c43a
Xakimov1610 <nosirbekxakimov01@gmail.com>
2022-09-13 23:35:49 +0500

    REAL Project

---
+++ FileSystemWatcherSample/src/Constants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderWatcherService
+{
+    public static class Constants
+    {
+        public const string ORDERS_FILE_EXTENSION = "*.order";
+        public const string ORDERSDB_CONN_STRING_NAME = "OrdersDb";
+    }
+}
---
+++ FileSystemWatcherSample/src/OrderWatcher.cs
@@ -0,0 +1,66 @@
+﻿using System.Configuration;
+
+namespace OrderWatcherService
+{
+    public class OrderWatcher
+    {
+        public static OrderWatcher Instance
+        {
+            get
+            {
+                return Nested.OrderWatcher;
+            }
+        }
+
+        private class Nested
+        {
+            static Nested() { }
+            internal static readonly OrderWatcher OrderWatcher = new OrderWatcher();
+        }
+
+        private string _ordersPath = string.Empty;
+        public void Init(string ordersPath)
+        {
+            _ordersPath = ordersPath;
+            CreateFileSystemWatcher();
+            ProcessExistingFiles();
+        }
+
+        private void ProcessExistingFiles()
+        {
+            var orderfiles = Directory.GetFiles(_ordersPath, Constants.ORDERS_FILE_EXTENSION);
+            foreach (var orderFile in orderfiles)
+            {
+                OrderManager.Instance.ProcessNewOrders(orderFile);
+            }
+        }
+
+        private void CreateFileSystemWatcher()
+        {
+            var watcher = new FileSystemWatcher();
+            watcher.Path = _ordersPath;
+            watcher.IncludeSubdirectories = false;
+            watcher.NotifyFilter = NotifyFilters.CreationTime |
+                                   NotifyFilters.FileName |
+                                   NotifyFilters.LastWrite |
+                                   NotifyFilters.Size;
+
+            watcher.Filter = Constants.ORDERS_FILE_EXTENSION;
+            watcher.Created += OnCreated;
+            watcher.Renamed += OnRenamed;
+            watcher.EnableRaisingEvents = true;
+        }
+
+        private void OnCreated(object sender, FileSystemEventArgs e)
+        {
+            OrderManager.Instance.ProcessNewOrders(e.FullPath);
+        }
+
+        private void OnRenamed(object sender, RenamedEventArgs e)
+        {
+           OrderManager.Instance.ProcessNewOrders(e.FullPath);
+        }
+
+
+    }
+}
