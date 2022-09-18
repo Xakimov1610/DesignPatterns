1794efbf9e74e0d5d897fbb6c56788aad45f484b
Xakimov1610 <nosirbekxakimov01@gmail.com>
2022-09-18 23:33:37 +0500

    Builder

---
+++ Builder/src/ITeaBuilder.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualDars.DesignPatterns.Builder
+{
+    public interface ITeaBuilder
+    {
+        void CreateTea();
+        void SetTeaType();
+        void SetWater();
+        void SetMilk();
+        void SetSugar();
+        void SetLemon();
+        void SetIce();
+        Tea GetTea();
+    }
+}
---
+++ Builder/src/Tea.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualDars.DesignPatterns.Builder
+{
+    public class Tea
+    {
+        public string Name { get; set; }
+        public double Water { get; set; }
+        public double Milk { get; set; }
+        public double Sugar { get; set; }
+        public double Lemon { get; set; }
+        public double Ice { get; set; }
+        public string Type { get; set; }
+
+        public string Show()
+        {
+            return $"{Name} tayyor bo'ldi!";
+        }
+    }
+}
