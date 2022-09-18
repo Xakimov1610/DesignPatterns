adaf2c7263dbebad0725289af22be122128ebffe
Xakimov1610 <nosirbekxakimov01@gmail.com>
2022-09-18 23:37:58 +0500

    Using Tea as a Simple

--- Builder/src/IFluentTeaBuilder.cs
+++ Builder/src/IFluentTeaBuilder.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace VirtualDars.DesignPatterns.Builder
+namespace Builder.src
 {
     public interface IFluentTeaBuilder
     {
