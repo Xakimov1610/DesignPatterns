eec7125b101f989f3a5535e6de9f937ac28fe112
Xakimov1610 <nosirbekxakimov01@gmail.com>
2022-09-24 23:46:27 +0500

    File System WAtcher Changed

--- Builder/Program.cs
+++ Builder/Program.cs
@@ -1,53 +1,53 @@
-﻿// using System.Data.SqlClient;
-// using Builder.Fluent;
-// using Builder.Traditional;
+﻿using System.Data.SqlClient;
+using Builder.Fluent;
+using Builder.Traditional;
 
-// // an'anaviy builder
-// var teaDirector = new TeaDirector();
+// an'anaviy builder
+var teaDirector = new TeaDirector();
 
-// var lemonTeaBuilder = new LemonTeaBuilder();
-// var tea1 = teaDirector.MakeTea(lemonTeaBuilder);
-// WriteLine(tea1.Show());
-// WriteLine();
+var lemonTeaBuilder = new LemonTeaBuilder();
+var tea1 = teaDirector.MakeTea(lemonTeaBuilder);
+WriteLine(tea1.Show());
+WriteLine();
 
-// var greenTeaBuilder = new GreenTeaBuilder();
-// var tea2 = teaDirector.MakeTea(greenTeaBuilder);
-// WriteLine(tea2.Show());
-// WriteLine();
-
-
-// // fluent builder
-// var fluentTeaDirector = new FluentTeaDirector();
-
-// var fluentCappBuilder = new FluentLemonTeaBuilder();
-// var tea3 = fluentTeaDirector.MakeTea(fluentCappBuilder);
-// WriteLine(tea3.Show());
-// WriteLine();
-
-// var fluentBlackTeaBuilder = new FluentBlackTeaBuilder();
-// var tea4 = fluentTeaDirector.MakeTea(fluentBlackTeaBuilder);
-// WriteLine(tea4.Show());
-// WriteLine();
+var greenTeaBuilder = new GreenTeaBuilder();
+var tea2 = teaDirector.MakeTea(greenTeaBuilder);
+WriteLine(tea2.Show());
+WriteLine();
 
 
+// fluent builder
+var fluentTeaDirector = new FluentTeaDirector();
 
-// // Builder patterniga .Net dan misolllar: StringBuilder va SqlConnectionStringBuilder
-// var sb = new StringBuilder();
-// sb.Append("Salom");
-// sb.Append(" ");
-// sb.Append("Dunyo!");
-// WriteLine(sb.ToString());
-// WriteLine();
+var fluentCappBuilder = new FluentLemonTeaBuilder();
+var tea3 = fluentTeaDirector.MakeTea(fluentCappBuilder);
+WriteLine(tea3.Show());
+WriteLine();
+
+var fluentBlackTeaBuilder = new FluentBlackTeaBuilder();
+var tea4 = fluentTeaDirector.MakeTea(fluentBlackTeaBuilder);
+WriteLine(tea4.Show());
+WriteLine();
 
 
-// var cb = new SqlConnectionStringBuilder("Data Source=myServer;Initial Catalog=myDatabase;Integrated Security=True;");
-// var dbName = cb.InitialCatalog;
-// WriteLine(dbName);
-// cb.DataSource = "yourServer";
-// cb.InitialCatalog = "yourDatabase";
-// cb.IntegratedSecurity = false;
-// string updatedConnString = cb.ConnectionString;
-// Console.WriteLine(updatedConnString);
-// WriteLine();
 
-// ReadLine();
+// Builder patterniga .Net dan misolllar: StringBuilder va SqlConnectionStringBuilder
+var sb = new StringBuilder();
+sb.Append("Salom");
+sb.Append(" ");
+sb.Append("Dunyo!");
+WriteLine(sb.ToString());
+WriteLine();
+
+
+var cb = new SqlConnectionStringBuilder("Data Source=myServer;Initial Catalog=myDatabase;Integrated Security=True;");
+var dbName = cb.InitialCatalog;
+WriteLine(dbName);
+cb.DataSource = "yourServer";
+cb.InitialCatalog = "yourDatabase";
+cb.IntegratedSecurity = false;
+string updatedConnString = cb.ConnectionString;
+Console.WriteLine(updatedConnString);
+WriteLine();
+
+ReadLine();
